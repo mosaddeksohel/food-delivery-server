432435a6fc3ab923ce076d402e1bd746b1134b37
Mosaddek Sohel <tanjimsohel>
2022-02-06 20:29:21 +0600

    connect database

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+
+const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10dvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('Connected to the database')
+
+    }
+    finally {
+        // await client.close();
+    }
+
+
+
+}
+run().catch(console.dir);
 
 
 
