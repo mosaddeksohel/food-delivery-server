b439a4c437e04c96181f4f0a9ba6f8ffceec9a5f
Mosaddek Sohel <tanjimsohel>
2022-02-07 00:25:02 +0600

    find data

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { get } = require('express/lib/response');
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config()
 
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+
+
+// middle wire 
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10dvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -12,7 +20,22 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('Connected to the database')
+        const database = client.db('delivery-food');
+        const productCollection = database.collection('products')
+        // get all data
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find({});
+            const product = await cursor.toArray();
+            res.send(product)
+        });
+
+
+        // Post Data
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product)
+            res.json(result)
+        })
 
     }
     finally {
@@ -29,7 +52,7 @@ run().catch(console.dir);
 
 
 app.get('/', (res, req) => {
-    res.send('Run the delivery porjects');
+    req.send('Run the delivery porjects');
 })
 
 
